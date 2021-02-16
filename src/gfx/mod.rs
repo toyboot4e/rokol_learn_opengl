429e6be8e8249784d934cb0fc58ba38e5d00d0a1
toyboot4e <toyboot4e@gmail.com>
2021-02-17 02:20:40 +0900

    add tex.rs (WIP)

--- src/gfx/mod.rs
+++ src/gfx/mod.rs
@@ -1,9 +1,13 @@
 /*!
 Higher-level objects on [`rokol::gfx`]
+
+They clear resource on `drop` and wrap `rokol` functions in methods.
 */
 
 mod mesh;
 mod shader;
+mod tex;
 
 pub use mesh::{DynamicMesh, StaticMesh};
 pub use shader::Shader;
+pub use tex::{RenderTexture2d, Texture2dDrop};
---
+++ src/gfx/tex.rs
@@ -0,0 +1,189 @@
+use {
+    image::GenericImageView,
+    rokol::gfx::{self as rg, BakedResource},
+    std::path::Path,
+};
+
+/// Image loading result
+pub type Result<T> = image::ImageResult<T>;
+
+/// Owned or borrowed pixels (specific `Cow` for `TextureBuilder`)
+#[derive(Debug)]
+enum Pixels<'a> {
+    /// Owned pixels loaded from storage or memory
+    Dyn(image::DynamicImage),
+    /// Borrowed pixels loaded from memory
+    Static(&'a [u8]),
+}
+
+impl<'a> Pixels<'a> {
+    pub fn as_bytes(&'a self) -> &'a [u8] {
+        match self {
+            Self::Dyn(img) => img.as_bytes(),
+            Self::Static(bytes) => bytes,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct TextureBuilder<'a> {
+    pixels: Pixels<'a>,
+    size: [u32; 2],
+    pub filter: rg::Filter,
+    pub wrap: rg::Wrap,
+}
+
+impl TextureBuilder<'static> {
+    pub fn from_path(path: &Path) -> Result<Self> {
+        let img = image::open(path)?;
+        Ok(Self::from_dyn_img(img))
+    }
+
+    pub fn from_encoded_bytes(mem: &[u8]) -> Result<Self> {
+        let img = image::load_from_memory(mem)?;
+        Ok(Self::from_dyn_img(img))
+    }
+
+    fn from_dyn_img(img: image::DynamicImage) -> Self {
+        let size = [img.width(), img.height()];
+        Self {
+            pixels: Pixels::Dyn(img),
+            size,
+            filter: rg::Filter::Linear,
+            wrap: rg::Wrap::ClampToEdge,
+        }
+    }
+}
+
+impl<'a> TextureBuilder<'a> {
+    pub fn from_pixels(pixels: &'a [u8], w: u32, h: u32) -> Self {
+        Self {
+            pixels: Pixels::Static(pixels),
+            size: [w, h],
+            filter: rg::Filter::Linear,
+            wrap: rg::Wrap::ClampToEdge,
+        }
+    }
+
+    fn img_desc(&self) -> rg::ImageDesc {
+        let pixels = self.pixels.as_bytes();
+        rg::ImageDesc {
+            type_: rg::ImageType::Dim2 as u32,
+            width: self.size[0] as i32,
+            height: self.size[1] as i32,
+            min_filter: self.filter as u32,
+            mag_filter: self.filter as u32,
+            wrap_u: self.wrap as u32,
+            wrap_v: self.wrap as u32,
+            wrap_w: self.wrap as u32,
+            ..Default::default()
+        }
+    }
+
+    pub fn filter(&mut self, filter: rg::Filter) -> &mut Self {
+        self.filter = filter;
+        self
+    }
+
+    pub fn wrap(&mut self, wrap: rg::Wrap) -> &mut Self {
+        self.wrap = wrap;
+        self
+    }
+}
+
+// fn image_desc(pixels: &[u8], w: u32, h: u32, filter: rg::Filter, wrap: rg::Wrap) -> rg::ImageDesc {
+//     let mut desc = image_desc_2d(w, h, filter, wrap);
+//     desc.render_target = false;
+//     desc.usage = rg::ResourceUsage::Immutable as u32;
+//     desc.data.subimage[0][0] = pixels.into();
+//     desc
+// }
+
+// fn target_desc(w: u32, h: u32, filter: rg::Filter, wrap: rg::Wrap) -> rg::ImageDesc {
+//     let mut desc = image_desc_2d(w, h, filter, wrap);
+//     desc.render_target = true;
+//     // TODO:
+//     // desc.usage = rg::ResourceUsage::Dynamic as u32;
+//     desc
+// }
+
+// fn stencil_desc(w: u32, h: u32, filter: rg::Filter, wrap: rg::Wrap) -> rg::ImageDesc {
+//     let mut desc = image_desc_2d(w, h, filter, wrap);
+//     desc.render_target = true;
+//     desc.pixel_format = rg::PixelFormat::DepthStencil as u32;
+//     // TODO:
+//     // desc.usage = rg::ResourceUsage::Dynamic as u32;
+//     // desc.sample_count = 1;
+//     desc
+// }
+
+/// Owned 2D texture
+#[derive(Debug, Default)]
+pub struct Texture2dDrop {
+    img: rg::Image,
+    w: u32,
+    h: u32,
+}
+
+impl Drop for Texture2dDrop {
+    fn drop(&mut self) {
+        rg::Image::destroy(self.img);
+    }
+}
+
+impl Texture2dDrop {
+    pub fn w(&self) -> u32 {
+        self.w
+    }
+    pub fn h(&self) -> u32 {
+        self.w
+    }
+    pub fn size(&self) -> [u32; 2] {
+        [self.w, self.h]
+    }
+    pub fn img(&self) -> rg::Image {
+        self.img
+    }
+}
+
+/// Off-screen 2D rendering target
+#[derive(Debug, Default)]
+pub struct RenderTexture2d {
+    /// Render target texture binded to the rendering pass
+    tex: Texture2dDrop,
+    /// Off-screen rendering pass
+    pass: rg::Pass,
+}
+
+impl Drop for RenderTexture2d {
+    fn drop(&mut self) {
+        rg::Pass::destroy(self.pass);
+    }
+}
+
+impl RenderTexture2d {
+    /// [`rokol::gfx::Pass`] for off-screen rendering
+    pub fn pass(&self) -> rg::Pass {
+        self.pass
+    }
+
+    pub fn tex(&self) -> &Texture2dDrop {
+        &self.tex
+    }
+
+    pub fn w(&self) -> u32 {
+        self.tex.w
+    }
+
+    pub fn h(&self) -> u32 {
+        self.tex.h
+    }
+
+    pub fn size(&self) -> [u32; 2] {
+        [self.tex.w, self.tex.h]
+    }
+
+    pub fn img(&self) -> rg::Image {
+        self.tex.img
+    }
+}
